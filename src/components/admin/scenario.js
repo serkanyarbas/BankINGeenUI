26a2e5750a8e256a711227d93053995d5f1ddb19
serkanyarbas <serkanyarbas@gmail.com>
2024-03-14 03:13:39 +0300

    service integration

--- src/components/admin/scenario.js
+++ src/components/admin/scenario.js
@@ -6,6 +6,7 @@ import '@lion/ui/define/lion-fieldset.js';
 import '@lion/ui/define/lion-checkbox.js';
 import '@lion/ui/define/lion-listbox.js';
 import { SessionMixin } from '../../mixin/sessionMixin.js';
+import '@lion/ui/define/lion-textarea.js';
 
 class Scenario extends SessionMixin(LitElement) {
   // This will make sure the component does not have a shadow root
@@ -16,38 +17,115 @@ class Scenario extends SessionMixin(LitElement) {
   static styles = css``;
 
   static properties = {
-    listboxData: { type: Array },
+    listboxData: { type: Array, reflect: true },
     columnData: { type: Array },
   };
 
+  constructor() {
+    super();
+    this.listboxData = [];
+  }
+
   submit() {
-    console.log(this.columnData);
+    debugger;
+
+    const data = {
+      scenarioName: this.querySelector('#scenarioName').value,
+      tableName: this.querySelector('#tableSelection').value,
+      columns: this.columnData,
+      makerGroupId: this.querySelector('#starterRole').modelValue,
+      checkerGroupId: this.querySelector('#approverRole').modelValue,
+    };
+
+    fetch('http://localhost:8080/admin/table/list/v1', {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error, status = ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        debugger;
+        console.log(data);
+      });
+  }
+
+  firstUpdated() {
+    fetch('http://localhost:8080/admin/table/list/v1', {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: null, // body data type must match "Content-Type" header
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error, status = ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.listboxData = data.tableNames;
+      });
   }
 
   render() {
-    this.listboxData = ['deneme', 'test'];
-    this.columnData = [
-      {
-        name: 'column1',
-        isKey: true,
-        editable: false,
-        view: true,
-        description: 'description',
-      },
-      {
-        name: 'column2',
-        isKey: false,
-        editable: true,
-        view: true,
-        description: 'description',
-      },
-    ];
-    this.roles = ['A', 'B', 'C'];
+    this.roles = ['1', '2', '3'];
 
     return html`
       <lion-fieldset name="tableGroup" label="Table">
-        <lion-combobox name="combo" label="Table Name">
-          ${this.listboxData.map(
+        <lion-combobox
+          id="tableSelection"
+          name="combo"
+          label="Table Name"
+          @model-value-changed="${e => {
+            debugger;
+
+            fetch('http://localhost:8080/admin/table/column/list/v1', {
+              method: 'POST', // *GET, POST, PUT, DELETE, etc.
+              mode: 'cors', // no-cors, *cors, same-origin
+              cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+              credentials: 'same-origin', // include, *same-origin, omit
+              headers: {
+                'Content-Type': 'application/json',
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+              },
+              redirect: 'follow', // manual, *follow, error
+              referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+              body: JSON.stringify({
+                tableName: 'ss_resource',
+              }), // body data type must match "Content-Type" header
+            })
+              .then(response => {
+                if (!response.ok) {
+                  throw new Error(`HTTP error, status = ${response.status}`);
+                }
+                return response.json();
+              })
+              .then(data => {
+                this.columnData = data.tableColumns;
+              });
+          }}"
+        >
+          ${this.listboxData?.map(
             (entry, i) =>
               html`
                 <lion-option .checked="${i === 0}" .choiceValue="${entry}"
@@ -57,7 +135,13 @@ class Scenario extends SessionMixin(LitElement) {
           )}
         </lion-combobox>
 
-        <lion-input label="View Name"></lion-input>
+        <lion-input id="scenarioName" label="Senaryo Adı"></lion-input>
+
+        <lion-textarea
+          id="scenarioDesc"
+          label="Senaryo Açıklaması"
+          max-rows="4"
+        ></lion-textarea>
 
         <div class="table-responsive">
           <table class="table table-striped table-sm">
@@ -66,33 +150,47 @@ class Scenario extends SessionMixin(LitElement) {
                 <th scope="col">Column</th>
                 <th scope="col">Key</th>
                 <th scope="col">Edit</th>
-                <th scope="col">View</th>
+                <th scope="col">Visible</th>
               </tr>
             </thead>
             <tbody>
-              ${this.columnData.map(
+              ${this.columnData?.map(
                 entry =>
                   html`
-                <tr>
-                  <td>${entry.name}</td>
-                  <td><input type="checkbox" .checked=${entry.isKey}></lion-checkbox> </td>
-                  <td><input type="checkbox" .checked=${entry.editable}></td>
-                  <td><lion-input>${entry.description}</lion-input> </td></td>
-                </tr>
+                    <tr>
+                      <td>${entry.columnName}</td>
+                      <td>
+                        <input type="checkbox" .checked=${entry.isPrimaryKey} />
+                      </td>
+                      <td>
+                        <input type="checkbox" .checked=${entry.isEditable} />
+                      </td>
+                      <td>
+                        <input type="checkbox" .checked=${entry.isVisible} />
+                      </td>
+                    </tr>
                   `
               )}
             </tbody>
           </table>
         </div>
 
-        <lion-listbox name="starterRoles" label="Giriş Rolleri" multiple-choice>
+        <lion-listbox
+          id="starterRole"
+          name="starterRoles"
+          label="Giriş Rolleri"
+        >
           ${this.roles.map(
             entry =>
               html` <lion-option .choiceValue=${entry}>${entry}</lion-option> `
           )}
         </lion-listbox>
 
-        <lion-listbox name="approverRoles" label="Onay Rolleri" multiple-choice>
+        <lion-listbox
+          id="approverRole"
+          name="approverRoles"
+          label="Onay Rolleri"
+        >
           ${this.roles.map(
             entry =>
               html` <lion-option .choiceValue=${entry}>${entry}</lion-option> `
--- src/components/login/bank-login.js
+++ src/components/login/bank-login.js
@@ -4,22 +4,10 @@ import { Router } from '@vaadin/router';
 
 class Home extends LitElement {
   static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--hack-demo-background-color);
-    }
-
     .text-center {
-      text-align: center !important;
+      margin: auto;
+      width: 50%;
+      padding: 10px;
     }
 
     .text-center .form-signin {
